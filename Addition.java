715775a7e5c570429d225977a32b162d9694e108
HackLoading <146710486+HackLoading@users.noreply.github.com>
2025-04-29 20:29:21 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
